67a07d0825cd1cc78ab628aa86391051a1cc3542
Yegor <edeykin85@gmail.com>
2023-01-10 15:50:43 +0300

    second try

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -14,7 +14,7 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void createUsersTable() {
-        String sql = "CREATE TABLE IF NOT EXISTS dbforpp114.users" +
+        String sql = "CREATE TABLE IF NOT EXISTS users" +
                 "(id int not null auto_increment," +
                 "name VARCHAR(50)," +
                 "lastName VARCHAR(50)," +
@@ -38,7 +38,7 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        String sql = "INSERT INTO dbforpp114.users(name, lastName, age) VALUES (?, ?, ?)";
+        String sql = "INSERT INTO users(name, lastName, age) VALUES (?, ?, ?)";
         try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
             preparedStatement.setString(1, name);
             preparedStatement.setString(2, lastName);
@@ -50,10 +50,10 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void removeUserById(long id) {
-        String sql = "DELETE FROM dbforpp114.users WHERE id = ?";
+        String sql = "DELETE FROM users WHERE id";
 
-        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
-            preparedStatement.setLong(1, id);
+        try (Statement statement = connection.createStatement()) {
+            statement.executeUpdate(sql);
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
@@ -61,7 +61,7 @@ public class UserDaoJDBCImpl implements UserDao {
 
     public List<User> getAllUsers() {
         List<User> listOfUsers = new ArrayList<>();
-        String sql = "SELECT * from dbforpp114.users";
+        String sql = "SELECT * from users";
 
         try (Statement statement = connection.createStatement()) {
             ResultSet resultSet = statement.executeQuery(sql);
@@ -81,7 +81,7 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void cleanUsersTable() {
-        String sql = "TRUNCATE dbforpp114.users";
+        String sql = "TRUNCATE users";
         try (Statement statement = connection.createStatement()) {
             statement.executeUpdate(sql);
         } catch (SQLException e) {
